88cedc5c29d94f91c516013dd226ed83761b0502
vikipretium <vighneshpasupathy@gmail.com>
2022-03-24 19:03:34 +0530

    add calculator structure

---
+++ src/components/Calculator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+// import './Calculator.css';
+
+export default class Calculator extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="calc-structure">
+        <div className="screen">
+          0
+        </div>
+        <div className="keyboard">
+          <button type="button" className="gray" id="ALLClear">AC</button>
+          <button type="button" className="gray" id="PlusMinus">+/-</button>
+          <button type="button" className="gray" id="percent">%</button>
+          <button type="button" className="orange" id="divide">&divide;</button>
+          <button type="button" className="gray" id="7">7</button>
+          <button type="button" className="gray" id="8">8</button>
+          <button type="button" className="gray" id="9">9</button>
+          <button type="button" className="orange" id="multiply">&times;</button>
+          <button type="button" className="gray" id="4">4</button>
+          <button type="button" className="gray" id="5">5</button>
+          <button type="button" className="gray" id="6">6</button>
+          <button type="button" className="orange" id="minus">&ndash;</button>
+          <button type="button" className="gray" id="1">1</button>
+          <button type="button" className="gray" id="2">2</button>
+          <button type="button" className="gray" id="3">3</button>
+          <button type="button" className="orange" id="plus">+</button>
+          <button type="button" className="gray" id="zero">0</button>
+          <button type="button" className="gray" id="dot">.</button>
+          <button type="button" className="orange" id="equal">=</button>
+        </div>
+      </div>
+    );
+  }
+}
