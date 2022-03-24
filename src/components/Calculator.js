909a6114acdec70ecea6461306b1210a6f51f48a
vikipretium <vighneshpasupathy@gmail.com>
2022-03-25 01:17:12 +0530

    add state and handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
   constructor(prop) {
     super(prop);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calc-structure">
         <div className="screen">
-          0
+          {total}
+          {operation}
+          {next}
         </div>
         <div className="keyboard">
-          <button type="button" className="gray" id="ALLClear">AC</button>
-          <button type="button" className="gray" id="PlusMinus">+/-</button>
-          <button type="button" className="gray" id="percent">%</button>
-          <button type="button" className="orange" id="divide">&divide;</button>
-          <button type="button" className="gray" id="7">7</button>
-          <button type="button" className="gray" id="8">8</button>
-          <button type="button" className="gray" id="9">9</button>
-          <button type="button" className="orange" id="multiply">&times;</button>
-          <button type="button" className="gray" id="4">4</button>
-          <button type="button" className="gray" id="5">5</button>
-          <button type="button" className="gray" id="6">6</button>
-          <button type="button" className="orange" id="minus">&ndash;</button>
-          <button type="button" className="gray" id="1">1</button>
-          <button type="button" className="gray" id="2">2</button>
-          <button type="button" className="gray" id="3">3</button>
-          <button type="button" className="orange" id="plus">+</button>
-          <button type="button" className="gray" id="zero">0</button>
-          <button type="button" className="gray" id="dot">.</button>
-          <button type="button" className="orange" id="equal">=</button>
+          <button type="button" onClick={this.handler} className="gray">AC</button>
+          <button type="button" onClick={this.handler} className="gray">+/-</button>
+          <button type="button" onClick={this.handler} className="gray">%</button>
+          <button type="button" onClick={this.handler} className="orange">÷</button>
+          <button type="button" onClick={this.handler} className="gray">7</button>
+          <button type="button" onClick={this.handler} className="gray">8</button>
+          <button type="button" onClick={this.handler} className="gray">9</button>
+          <button type="button" onClick={this.handler} className="orange">x</button>
+          <button type="button" onClick={this.handler} className="gray">4</button>
+          <button type="button" onClick={this.handler} className="gray">5</button>
+          <button type="button" onClick={this.handler} className="gray">6</button>
+          <button type="button" onClick={this.handler} className="orange">-</button>
+          <button type="button" onClick={this.handler} className="gray">1</button>
+          <button type="button" onClick={this.handler} className="gray">2</button>
+          <button type="button" onClick={this.handler} className="gray">3</button>
+          <button type="button" onClick={this.handler} className="orange">+</button>
+          <button type="button" onClick={this.handler} className="gray" id="zero">0</button>
+          <button type="button" onClick={this.handler} className="gray">.</button>
+          <button type="button" onClick={this.handler} className="orange">=</button>
         </div>
       </div>
     );
