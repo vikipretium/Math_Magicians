b9068b6fb9ecf84baa65d5147b94d237ee81588a
vikipretium <vighneshpasupathy@gmail.com>
2022-03-25 14:21:23 +0530

    Revert "change class to function base"
    This reverts commit 47968ec972862d7fbd6193e7093c9f523f46ef73.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-const Calculator = () => {
-  const [state, setState] = useState({ total: 0, next: null, operation: null });
+export default class Calculator extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handler = this.handler.bind(this);
+  }
 
-  const handler = (e) => {
-    setState((state) => calculate(state, e.target.textContent));
-  };
-  return (
-    <div className="calc-structure">
-      <div className="screen">
-        {state.total}
-        {state.operation}
-        {state.next}
+  handler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div className="calc-structure">
+        <div className="screen">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="keyboard">
+          <button type="button" onClick={this.handler} className="gray">AC</button>
+          <button type="button" onClick={this.handler} className="gray">+/-</button>
+          <button type="button" onClick={this.handler} className="gray">%</button>
+          <button type="button" onClick={this.handler} className="orange">÷</button>
+          <button type="button" onClick={this.handler} className="gray">7</button>
+          <button type="button" onClick={this.handler} className="gray">8</button>
+          <button type="button" onClick={this.handler} className="gray">9</button>
+          <button type="button" onClick={this.handler} className="orange">x</button>
+          <button type="button" onClick={this.handler} className="gray">4</button>
+          <button type="button" onClick={this.handler} className="gray">5</button>
+          <button type="button" onClick={this.handler} className="gray">6</button>
+          <button type="button" onClick={this.handler} className="orange">-</button>
+          <button type="button" onClick={this.handler} className="gray">1</button>
+          <button type="button" onClick={this.handler} className="gray">2</button>
+          <button type="button" onClick={this.handler} className="gray">3</button>
+          <button type="button" onClick={this.handler} className="orange">+</button>
+          <button type="button" onClick={this.handler} className="gray" id="zero">0</button>
+          <button type="button" onClick={this.handler} className="gray">.</button>
+          <button type="button" onClick={this.handler} className="orange">=</button>
+        </div>
       </div>
-      <div className="keyboard">
-        <button type="button" onClick={handler} className="gray">AC</button>
-        <button type="button" onClick={handler} className="gray">+/-</button>
-        <button type="button" onClick={handler} className="gray">%</button>
-        <button type="button" onClick={handler} className="orange">÷</button>
-        <button type="button" onClick={handler} className="gray">7</button>
-        <button type="button" onClick={handler} className="gray">8</button>
-        <button type="button" onClick={handler} className="gray">9</button>
-        <button type="button" onClick={handler} className="orange">x</button>
-        <button type="button" onClick={handler} className="gray">4</button>
-        <button type="button" onClick={handler} className="gray">5</button>
-        <button type="button" onClick={handler} className="gray">6</button>
-        <button type="button" onClick={handler} className="orange">-</button>
-        <button type="button" onClick={handler} className="gray">1</button>
-        <button type="button" onClick={handler} className="gray">2</button>
-        <button type="button" onClick={handler} className="gray">3</button>
-        <button type="button" onClick={handler} className="orange">+</button>
-        <button type="button" onClick={handler} className="gray" id="zero">0</button>
-        <button type="button" onClick={handler} className="gray">.</button>
-        <button type="button" onClick={handler} className="orange">=</button>
-      </div>
-    </div>
-  );
-};
-export default Calculator;
+    );
+  }
+}
