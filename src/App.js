84be648b72ecc6b172933bee83c85dd509899e24
vikipretium <vighneshpasupathy@gmail.com>
2022-03-31 15:21:36 +0530

    install react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import NavBar from './components/NavBar';
-<Route path="/Calculator" element={<Calculator />} />
 
 export default class App extends React.Component {
   constructor(props) {
