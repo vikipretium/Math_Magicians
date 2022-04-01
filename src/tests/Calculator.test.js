fbf9fa4045804cbe1444a4a658753d48a7ac9a37
vikipretium <vighneshpasupathy@gmail.com>
2022-04-01 17:56:10 +0530

    remove test files for equal contribution

--- src/tests/Calculator.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Calculator from '../components/Calculator';
-
-it('Calculator renders correctly', () => {
-  const tree = render(<Calculator />);
-  expect(tree).toMatchSnapshot();
-});
